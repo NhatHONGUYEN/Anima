29c10d9ae347353ac7d79c7e48d574447ca44500
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-02-12 15:49:18 +0100

    ✨ add caroussel for top anime

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,9 @@
-"use client";
-
-import { useState } from "react";
-import { useTopAnime } from "@/hooks/useTopAnime";
-import Pagination from "@/components/Pagination";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-
-type Anime = {
-  mal_id: number;
-  title: string;
-  synopsis: string;
-  score: number;
-  genres: { name: string }[];
-  images: {
-    jpg: { image_url: string };
-    webp: { image_url: string };
-  };
-};
+import TopAnimeSlider from "@/components/TopAnimeSlider";
 
 export default function Home() {
-  const [page, setPage] = useState(1);
-  const { data: topAnime, error, isLoading, isFetching } = useTopAnime(page);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-
   return (
     <div>
-      <h1>Top Anime</h1>
-      <ul>
-        {topAnime.data.map((anime: Anime) => (
-          <li key={anime.mal_id} className="anime-item">
-            <Image
-              src={
-                anime.images.webp
-                  ? anime.images.webp.image_url
-                  : anime.images.jpg.image_url
-              }
-              alt={anime.title}
-              className="anime-image"
-              width={200}
-              height={300}
-            />
-            <div className="anime-details">
-              <h2>{anime.title}</h2>
-              <p>{anime.synopsis}</p>
-              <p>
-                <strong>Genres:</strong>{" "}
-                {anime.genres.map((genre) => genre.name).join(", ")}
-              </p>
-              <p>
-                <strong>Score:</strong> {anime.score}
-              </p>
-              <div className="anime-buttons">
-                <Button>Watch</Button>
-                <Button>Info</Button>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <Pagination page={page} isFetching={isFetching} onPageChange={setPage} />
+      <TopAnimeSlider />
     </div>
   );
 }
---
+++ src/components/TopAnimeSlider.tsx
@@ -0,0 +1,154 @@
+"use client";
+
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { useEffect, useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { useTopAnime } from "@/hooks/useTopAnime";
+import Image from "next/image";
+import Link from "next/link";
+
+type Anime = {
+  mal_id: number;
+  title: string;
+  synopsis: string;
+  score: number;
+  genres: { name: string }[];
+  images: {
+    jpg: { image_url: string };
+    webp: { image_url: string };
+  };
+};
+
+export default function TopAnimeSlider() {
+  const [carouselApi, setCarouselApi] = useState<CarouselApi>();
+  const [canScrollPrev, setCanScrollPrev] = useState(false);
+  const [canScrollNext, setCanScrollNext] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    if (!carouselApi) {
+      return;
+    }
+    const updateSelection = () => {
+      setCanScrollPrev(carouselApi.canScrollPrev());
+      setCanScrollNext(carouselApi.canScrollNext());
+      setCurrentSlide(carouselApi.selectedScrollSnap());
+    };
+    updateSelection();
+    carouselApi.on("select", updateSelection);
+    return () => {
+      carouselApi.off("select", updateSelection);
+    };
+  }, [carouselApi]);
+
+  const { data: topAnime, error, isLoading } = useTopAnime(1);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <section className="py-32">
+      <div className="container max-w-6xl mx-auto ">
+        <div className="mb-8 flex items-end justify-between md:mb-14 lg:mb-16">
+          <div className="flex  flex-col gap-4">
+            <p className="max-w-lg text-muted-foreground">Trending Movies</p>
+          </div>
+          <div className="hidden shrink-0 gap-2 md:flex">
+            <Button
+              size="icon"
+              variant="ghost"
+              onClick={() => {
+                carouselApi?.scrollPrev();
+              }}
+              disabled={!canScrollPrev}
+              className="disabled:pointer-events-auto"
+            >
+              <ArrowLeft className="size-5" />
+            </Button>
+            <Button
+              size="icon"
+              variant="ghost"
+              onClick={() => {
+                carouselApi?.scrollNext();
+              }}
+              disabled={!canScrollNext}
+              className="disabled:pointer-events-auto"
+            >
+              <ArrowRight className="size-5" />
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="w-full">
+        <Carousel
+          setApi={setCarouselApi}
+          opts={{
+            breakpoints: {
+              "(max-width: 768px)": {
+                dragFree: true,
+              },
+            },
+          }}
+        >
+          <CarouselContent className="ml-5 mr-5 2xl:ml-[calc(50vw-700px+20px)] 2xl:mr-[calc(50vw-700px+20px)]">
+            {topAnime.data.map((anime: Anime) => (
+              <CarouselItem
+                key={anime.mal_id}
+                className="max-w-[320px] pl-[20px] lg:max-w-[360px]"
+              >
+                <Link href={"/"} className="group rounded-xl">
+                  <div className="group relative h-full min-h-[27rem] max-w-full overflow-hidden rounded-xl md:aspect-[5/4] lg:aspect-[16/9]">
+                    <Image
+                      src={
+                        anime.images.webp
+                          ? anime.images.webp.image_url
+                          : anime.images.jpg.image_url
+                      }
+                      alt={anime.title}
+                      width={400}
+                      height={450}
+                      quality={100}
+                      className="absolute h-full w-full object-cover object-center transition-transform duration-300 group-hover:scale-105"
+                    />
+                    <div className="absolute inset-0 h-full bg-[linear-gradient(hsl(var(--primary)/0),hsl(var(--primary)/0.4),hsl(var(--primary)/0.8)_100%)] mix-blend-multiply" />
+                    <div className="absolute inset-x-0 bottom-0 flex flex-col items-start p-6 text-primary-foreground md:p-8">
+                      <div className="mb-2 pt-4 text-xl font-semibold md:mb-3 md:pt-4 lg:pt-4">
+                        {anime.title}
+                      </div>
+                      <div className="mb-8 line-clamp-2 md:mb-12 lg:mb-9">
+                        {anime.synopsis}
+                      </div>
+                      <div className="flex items-center text-sm">
+                        Read more{" "}
+                        <ArrowRight className="ml-2 size-5 transition-transform group-hover:translate-x-1" />
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+        <div className="mt-8 flex justify-center gap-2">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <button
+              key={index}
+              className={`h-2 w-2 rounded-full transition-colors ${
+                currentSlide === index ? "bg-primary" : "bg-primary/20"
+              }`}
+              onClick={() => carouselApi?.scrollTo(index)}
+              aria-label={`Go to slide ${index + 1}`}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
