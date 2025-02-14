1e65b1cd6d4ea72d161c88945221af3c16a59097
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-02-14 12:43:41 +0100

    💄 add load more

--- src/components/AnimeList.tsx
+++ src/components/AnimeList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTopAnime } from "@/hooks/useTopAnime";
 import AnimeCard from "@/components/AnimeCard";
 import { Anime } from "@/lib/types";
@@ -16,28 +16,43 @@ export default function AnimeList({
   title,
   description,
 }: AnimeListProps) {
-  const { data, error, isLoading } = useTopAnime(filter);
+  const [page, setPage] = useState(1);
+  const [allAnime, setAllAnime] = useState<Anime[]>([]);
+  const { data, error, isLoading } = useTopAnime(filter, page);
 
-  if (isLoading) return <div>Loading...</div>;
+  useEffect(() => {
+    if (data?.data) {
+      setAllAnime((prevAnime) => [...prevAnime, ...data.data]);
+    }
+  }, [data]);
+
+  if (isLoading && page === 1) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
-  const animeList = data?.data;
-  if (!Array.isArray(animeList)) return <div>No anime found</div>;
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
     <section className="py-16">
-      <div className="container mx-auto flex flex-col items-center gap-16 lg:px-16">
-        <div className="text-center">
+      <div className="container max-w-6xl mx-auto">
+        <div className="text-center flex flex-col items-center mb-8">
           <h1 className="text-2xl font-bold mb-4">{title}</h1>
           <p className="text-muted-foreground lg:max-w-3xl lg:text-lg">
             {description}
           </p>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {animeList.map((anime: Anime, index) => (
+          {allAnime.map((anime: Anime, index) => (
             <AnimeCard key={`${anime.mal_id}-${index}`} anime={anime} />
           ))}
         </div>
+        <button
+          onClick={handleLoadMore}
+          className="mt-8 px-4 py-2 bg-primary text-white rounded-lg"
+        >
+          Load More
+        </button>
       </div>
     </section>
   );
