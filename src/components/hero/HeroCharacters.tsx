9a245ad61f7c28f8a70ebab844e5b7355aa57a55
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-02-14 19:08:31 +0100

    💄 change the size of image

--- src/components/hero/HeroCharacters.tsx
+++ src/components/hero/HeroCharacters.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import React, { useState } from "react";
+import Image from "next/image";
 import { Characters } from "@/lib/types";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 
 type HeroCharactersProps = {
@@ -10,54 +10,53 @@ type HeroCharactersProps = {
 };
 
 export default function HeroCharacters({ characters }: HeroCharactersProps) {
-  const [visibleCount, setVisibleCount] = useState(6);
+  const [visibleCount, setVisibleCount] = useState(4);
 
   const displayedCharacters = characters.slice(0, visibleCount);
 
   const handleShowMore = () => {
-    setVisibleCount((prevCount) => prevCount + 6);
+    setVisibleCount((prevCount) => prevCount + 4);
   };
 
   const handleShowLess = () => {
-    setVisibleCount((prevCount) => Math.max(prevCount - 6, 6));
+    setVisibleCount((prevCount) => Math.max(prevCount - 4, 4));
   };
 
   return (
-    <div className="relative lg:col-span-3">
-      <div className="absolute inset-px rounded-lg ring-1 ring-border bg-card max-lg:rounded-t-[2rem] lg:rounded-bl-[2rem]" />
+    <div className="relative py-16 lg:col-span-3">
+      <div className="absolute inset-px rounded-lg bg-card max-lg:rounded-t-[2rem] lg:rounded-bl-[2rem]" />
       <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-bl-[calc(2rem+1px)] min-h-[300px] lg:min-h-[400px]">
-        <div className="container mt-16 grid gap-x-8 gap-y-16 md:grid-cols-2 lg:grid-cols-3">
-          {displayedCharacters.map((characterObj, charIndex) => (
-            <div
-              key={`${characterObj.character.mal_id}-${charIndex}`}
-              className="flex flex-col items-center"
-            >
-              <Avatar className="mb-4 size-20 border md:mb-5 lg:size-24">
-                <AvatarImage
-                  src={
-                    characterObj.character.images?.webp?.image_url ||
-                    "/path/to/default/image.jpg"
-                  }
-                  alt={`Image of ${characterObj.character.name}`}
-                  className="object-cover"
-                />
-                <AvatarFallback> {characterObj.character.name}</AvatarFallback>
-              </Avatar>
-              <p className="text-center font-medium">
-                {characterObj.character.name}
-              </p>
-              <p className="text-center text-muted-foreground">
-                {characterObj.role}
-              </p>
-            </div>
-          ))}
+        <h1 className="mb-8 text-center font-bold">Characters</h1>
+        <div className="container grid gap-8 md:grid-cols-2">
+          {displayedCharacters.length > 0 ? (
+            displayedCharacters.map((characterObj, charIndex) => (
+              <div key={`${characterObj.character.mal_id}-${charIndex}`}>
+                <div className="relative w-full h-48">
+                  <Image
+                    alt={`Image of ${characterObj.character.name}`}
+                    src={
+                      characterObj.character.images?.webp?.image_url ||
+                      "/path/to/default/image.jpg"
+                    }
+                    className="rounded-lg  object-cover"
+                    layout="fill"
+                  />
+                </div>
+                <p className="py-8 text-center">
+                  {characterObj.character.name}
+                </p>
+              </div>
+            ))
+          ) : (
+            <p className="text-center">No characters found.</p>
+          )}
         </div>
         <div className="mt-4 flex justify-center space-x-4">
           {visibleCount < characters.length && (
             <Button onClick={handleShowMore}>Show More</Button>
           )}
-          {visibleCount > 6 && (
-            <Button variant={"outline"} onClick={handleShowLess}>
+          {visibleCount > 4 && (
+            <Button variant="outline" onClick={handleShowLess}>
               Show Less
             </Button>
           )}
--- src/components/hero/HeroVoiceActors.tsx
+++ src/components/hero/HeroVoiceActors.tsx
@@ -2,15 +2,15 @@
 
 import React, { useState } from "react";
 import { Characters } from "@/lib/types";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
+import Image from "next/image";
 
 type HeroVoiceActorsProps = {
   voiceActors: Characters["voice_actors"];
 };
 
 export default function HeroVoiceActors({ voiceActors }: HeroVoiceActorsProps) {
-  const [visibleCount, setVisibleCount] = useState(6);
+  const [visibleCount, setVisibleCount] = useState(4);
 
   const japaneseVoiceActors = voiceActors.filter(
     (actor) => actor.language === "Japanese"
@@ -19,49 +19,44 @@ export default function HeroVoiceActors({ voiceActors }: HeroVoiceActorsProps) {
   const displayedVoiceActors = japaneseVoiceActors.slice(0, visibleCount);
 
   const handleShowMore = () => {
-    setVisibleCount((prevCount) => prevCount + 6);
+    setVisibleCount((prevCount) => prevCount + 4);
   };
 
   const handleShowLess = () => {
-    setVisibleCount((prevCount) => Math.max(prevCount - 6, 6));
+    setVisibleCount((prevCount) => Math.max(prevCount - 4, 4));
   };
 
   return (
-    <div className="relative lg:col-span-3">
+    <div className="relative py-16 lg:col-span-3">
       <div className="absolute inset-px rounded-lg ring-1 ring-border bg-card max-lg:rounded-t-[2rem] lg:rounded-br-[2rem]" />
       <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-br-[calc(2rem+1px)] min-h-[300px] lg:min-h-[400px]">
-        <div className="container mt-16 grid gap-x-8 gap-y-16 md:grid-cols-2 lg:grid-cols-3">
+        <h1 className="mb-8  text-center font-bold ">Voice Actors</h1>
+        <div className="container grid gap-8 md:grid-cols-2">
           {displayedVoiceActors.length > 0 ? (
             displayedVoiceActors.map((actor, actorIndex) => (
-              <div
-                key={`${actor.person.mal_id}-${actorIndex}`}
-                className="flex flex-col items-center"
-              >
-                <Avatar className="mb-4 size-20 border md:mb-5 lg:size-24">
-                  <AvatarImage
-                    src={
-                      actor.person.images?.jpg?.image_url ||
-                      "/path/to/default/image.jpg"
-                    }
-                    alt={`Image of ${actor.person.name}`}
-                    className="object-cover"
-                  />
-                  <AvatarFallback> {actor.person.name}</AvatarFallback>
-                </Avatar>
-                <p className="text-center font-medium">
-                  {actor.person.name} ({actor.language})
-                </p>
+              <div key={`${actor.person.mal_id}-${actorIndex}`}>
+                <Image
+                  alt={`Image of ${actor.person.name}`}
+                  src={
+                    actor.person.images?.jpg?.image_url ||
+                    "/path/to/default/image.jpg"
+                  }
+                  className=" ring-1 ring-border w-full h-48  rounded-lg object-cover"
+                  width={300}
+                  height={450}
+                />
+                <p className="py-8 text-center">{actor.person.name}</p>
               </div>
             ))
           ) : (
-            <p>No Japanese voice actors found.</p>
+            <p className="text-center">No Japanese voice actors found.</p>
           )}
         </div>
         <div className="mt-4 flex justify-center space-x-4">
           {visibleCount < japaneseVoiceActors.length && (
             <Button onClick={handleShowMore}>Show More</Button>
           )}
-          {visibleCount > 6 && (
+          {visibleCount > 4 && (
             <Button variant="outline" onClick={handleShowLess}>
               Show Less
             </Button>
