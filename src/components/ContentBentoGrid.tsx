95a2765f49f2c725628bbd11f6b84532082030fc
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-02-22 02:06:50 +0100

    🐛 fix bug cause of fadeanimation

--- src/components/ContentBentoGrid.tsx
+++ src/components/ContentBentoGrid.tsx
@@ -12,7 +12,6 @@ import BentoGridVoiceActors from "./bentoGrid/BentoGridVoiceActors";
 import CustomButton from "./CustomButton";
 import { MoveLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
-import FADE_DOWN_ANIMATION from "@/animations/FADE_DOWN_ANIMATION";
 
 export default function ContentBentoGrid({
   mal_id,
@@ -38,56 +37,54 @@ export default function ContentBentoGrid({
 
   return (
     <section>
-      <FADE_DOWN_ANIMATION>
-        <div className="mx-auto container ">
-          <div className="mt-10">
-            <CustomButton
-              icon={MoveLeft}
-              label="Go Back"
-              onClick={() => router.back()}
-            />
-          </div>
-
-          <div className="mt-10 grid grid-cols-1 gap-4 lg:grid-cols-6 lg:grid-rows-1">
-            <BentoGridHeader
-              mal_id={mal_id}
-              title={title}
-              score={score}
-              type={type}
-              episode={episode}
-              duration={duration}
-              favorites={favorites}
-              rank={rank}
-              synopsis={synopsis}
-              themes={themes}
-            />
-            <BentoGridTrailer
-              title={title}
-              trailerEmbedUrl={trailerEmbedUrl}
-              trailerImageUrl={trailerImageUrl}
-            />
-            <BentoGridImage largeImageUrl={largeImageUrl} title={title} />
-            <BentoGridDetails
-              trailerImageUrl={trailerImageUrl}
-              title={title}
-              producers={producers}
-              licensors={licensors}
-              studios={studios}
-            />
-            <BentoGridEpisodes episodes={episodes} />
-            {characters && characters.length > 0 && (
-              <BentoGridCharacters characters={characters} />
-            )}
-            {characters && characters.length > 0 && (
-              <BentoGridVoiceActors
-                voiceActors={characters.flatMap(
-                  (character) => character.voice_actors
-                )}
-              />
-            )}
-          </div>
+      <div className="mx-auto container ">
+        <div className="mt-10">
+          <CustomButton
+            icon={MoveLeft}
+            label="Go Back"
+            onClick={() => router.back()}
+          />
         </div>
-      </FADE_DOWN_ANIMATION>
+
+        <div className="mt-10 grid grid-cols-1 gap-4 lg:grid-cols-6 lg:grid-rows-1">
+          <BentoGridHeader
+            mal_id={mal_id}
+            title={title}
+            score={score}
+            type={type}
+            episode={episode}
+            duration={duration}
+            favorites={favorites}
+            rank={rank}
+            synopsis={synopsis}
+            themes={themes}
+          />
+          <BentoGridTrailer
+            title={title}
+            trailerEmbedUrl={trailerEmbedUrl}
+            trailerImageUrl={trailerImageUrl}
+          />
+          <BentoGridImage largeImageUrl={largeImageUrl} title={title} />
+          <BentoGridDetails
+            trailerImageUrl={trailerImageUrl}
+            title={title}
+            producers={producers}
+            licensors={licensors}
+            studios={studios}
+          />
+          <BentoGridEpisodes episodes={episodes} />
+          {characters && characters.length > 0 && (
+            <BentoGridCharacters characters={characters} />
+          )}
+          {characters && characters.length > 0 && (
+            <BentoGridVoiceActors
+              voiceActors={characters.flatMap(
+                (character) => character.voice_actors
+              )}
+            />
+          )}
+        </div>
+      </div>
     </section>
   );
 }
